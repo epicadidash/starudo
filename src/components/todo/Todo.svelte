b93f0df1fd7b682429d11c5eeabc5b2c08e24b5f
epicadidash <dradwaitdash@gmail.com>
2023-09-10 00:39:14 +0530

    Finishing Component Todo  part 1

--- src/components/todo/Todo.svelte
+++ src/components/todo/Todo.svelte
@@ -8,13 +8,6 @@
   </div>
 <span>Hello </span>
 </div>
-<div class="todocontainer">
-  <div class="round">
-    <input type="checkbox" id="star" />
-    <label for="star"></label>
-  </div>
-<span>Hello </span>
-</div>
 <style>
   @import "./todo.css";
 </style>
