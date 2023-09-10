bd6c50a3ea6706ca16ac49832a2dfdaaa503039b
epicadidash <dradwaitdash@gmail.com>
2023-09-11 00:39:20 +0530

    A better working framework of Todo

--- src/components/todo/Todo.svelte
+++ src/components/todo/Todo.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
+export let id:number;
+export let title:string;
+export let status:string;
+export let expecteddate:Date;
+let currentdate:Date= new Date();
+let currentYear = currentdate.getFullYear();
+ let currentMonth = currentdate.getMonth() + 1;
+let  currentDay = currentdate.getDate();
 
 </script>
 <div class="todocontainer">
   <div class="round">
-    <input type="checkbox" id="checkbox" />
-    <label for="checkbox"></label>
+    <input type="checkbox" id={id?.toString()} />
+    <label for={id?.toString()}></label>
   </div>
-<span>Hello </span>
+<span>{title}</span>
 </div>
 <style>
   @import "./todo.css";
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
+<script lang="ts">
+     import Todo from '../components/todo/Todo.svelte';
+     let star:number = 24;
+</script>
+
+<Todo id={star} title="Hello"/>
 <style>
      @import './home.css';
 </style>
-<Todo />
-<script>
-     import Todo from '../components/todo/Todo.svelte';
-</script>
+
