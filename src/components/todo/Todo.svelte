7046443cb235c1ca9bdf0959c315daa6f0b3e569
epicadidash <dradwaitdash@gmail.com>
2023-09-11 23:55:57 +0530

    Basic working Todo Structure Achieved

--- src/components/todo/Todo.svelte
+++ src/components/todo/Todo.svelte
@@ -1,20 +1,32 @@
 <script lang="ts">
 export let id:number;
 export let title:string;
-export let status:string;
+export let originalstatus:string = "not completed ";
 export let expecteddate:Date;
 let currentdate:Date= new Date();
 let currentYear = currentdate.getFullYear();
  let currentMonth = currentdate.getMonth() + 1;
 let  currentDay = currentdate.getDate();
-
+let strike = false
+$: currentstatus = originalstatus
+function oncompletion(event:Event){
+  event.stopPropagation();
+  strike = !strike;
+  console.log(strike)
+  console.log(currentstatus)
+  if(currentstatus === "completed"){
+    currentstatus = "not completed"
+  }else{
+    currentstatus= "completed"
+  }
+}
 </script>
 <div class="todocontainer">
-  <div class="round">
+  <div class="round" >
     <input type="checkbox" id={id?.toString()} />
-    <label for={id?.toString()}></label>
+    <label on:click|capture={oncompletion } for={id?.toString()}></label>
   </div>
-<span>{title}</span>
+<span class={strike ? 'strikethrough' : ''}>{title}</span>
 </div>
 <style>
   @import "./todo.css";
